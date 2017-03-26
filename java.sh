a70e26f11efbbed05f1f783578bc88fbc18d8a31
aviggiano <agfviggiano@gmail.com>
2017-03-26 19:40:47 -0300

    update java.sh

--- java.sh
+++ java.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install -y default-jre default-jdk
-echo "export JAVA_HOME=$(sudo update-alternatives --config java | awk '{print $NF}' | head -1)" >> ~/.bash_custom
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt update
+sudo apt install -y oracle-java8-installer
+sudo apt install -y oracle-java8-set-default
+HAS_CHOICES=$(echo '' | sudo update-alternatives --config java | grep choice)
+if [ "$HAS_CHOICES" != "" ]; then
+    JAVA_HOME=$(echo '' | sudo update-alternatives --config java | grep '*' | grep -v choice | awk '{print $3}')
+else    
+    JAVA_HOME=$(sudo update-alternatives --config java | awk '{print $NF}' | head -1)
+fi
+echo "export JAVA_HOME=$JAVA_HOME" >> ~/.bash_custom
