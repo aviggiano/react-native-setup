29034e616aceac422145a154489a835aeb6060bc
aviggiano <agfviggiano@gmail.com>
2017-03-26 20:12:30 -0300

    update jdk

--- java.sh
+++ java.sh
@@ -6,7 +6,7 @@ sudo apt install -y oracle-java8-installer
 sudo apt install -y oracle-java8-set-default
 HAS_CHOICES=$(echo '' | sudo update-alternatives --config java | grep choice)
 if [ "$HAS_CHOICES" != "" ]; then
-    JAVA_HOME=$(echo '' | sudo update-alternatives --config java | grep '*' | grep -v choice | awk '{print $3}' | rev | cut --complement -d'/' -f1 | rev)
+    JAVA_HOME=$(echo '' | sudo update-alternatives --config java | grep '*' | grep -v choice | awk '{print $3}' | sed 's/\/jre.*//g')
 else    
     JAVA_HOME=$(sudo update-alternatives --config java | awk '{print $NF}' | head -1)
 fi
