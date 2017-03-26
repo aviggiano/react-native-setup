0aa9d314633974f322cc65c3d82d0174cd77af57
aviggiano <agfviggiano@gmail.com>
2017-03-26 19:02:18 -0300

    install java

--- java.sh
+++ java.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get update
 sudo apt-get install -y default-jre default-jdk
-
+echo "export JAVA_HOME=$(sudo update-alternatives --config java | awk '{print $NF}' | head -1)" >> ~/.bash_custom
