3f9bb41fbf2a35c93ab59b7954c86047104a9e2b
aviggiano <agfviggiano@gmail.com>
2017-03-26 20:08:09 -0300

    update

--- f8app.sh
+++ f8app.sh
@@ -11,4 +11,5 @@ yarn
 # adb reverse tcp:8081 tcp:8081   # required to ensure the Android app can
 # adb reverse tcp:8080 tcp:8080   # access the Packager
 
-
+# https://github.com/ReactWindows/f8app/blob/data/mongodb/db.zip
+# http://stackoverflow.com/questions/34243731/mongodb-cannot-start-server-the-default-storage-engine-wiredtiger-is-not-avai
