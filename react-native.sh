dc31b88bd55362793ba845ba8e7cf0dfc5b3f802
aviggiano <agfviggiano@gmail.com>
2017-03-26 22:35:20 -0300

    gradle

--- react-native.sh
+++ react-native.sh
@@ -24,4 +24,11 @@ echo "export ANDROID_HOME=\${HOME}/Android/Sdk" >> ~/.bash_custom
 echo "export PATH=\${PATH}:\${ANDROID_HOME}/tools" >> ~/.bash_custom
 echo "export PATH=\${PATH}:\${ANDROID_HOME}/platform-tools" >> ~/.bash_custom
 
+# gradle
+# https://facebook.github.io/react-native/docs/signed-apk-android.html
+echo "MYAPP_RELEASE_STORE_FILE=my-release-key.keystore" >> ~/.gradle/gradle.properties
+echo "MYAPP_RELEASE_KEY_ALIAS=my-key-alias" >> ~/.gradle/gradle.properties
+echo "MYAPP_RELEASE_STORE_PASSWORD=$(get_random_password)" >> ~/.gradle/gradle.properties
+echo "MYAPP_RELEASE_KEY_PASSWORD=$(get_random_password)" >> ~/.gradle/gradle.properties
+
 # https://discuss.circleci.com/t/android-build-tools-23-0-2-not-available/455
