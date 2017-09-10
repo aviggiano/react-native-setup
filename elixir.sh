3537d49cf16727067c04e9541fafa2a9f41706e0
Antonio Viggiano <agfviggiano@gmail.com>
2017-09-10 12:24:22 -0300

    Update setup

--- elixir.sh
+++ elixir.sh
@@ -4,8 +4,9 @@ set -eux
 
 # https://elixir-lang.org/install.html
 
+cd /tmp
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
-sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
+sudo apt-get update || true
+sudo apt-get install -y esl-erlang elixir
+cd -
